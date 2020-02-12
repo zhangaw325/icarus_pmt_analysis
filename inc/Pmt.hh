6478f53e7978b30462f338c328d26e156d151988
Andrea <andrea.scarpelli@cern.ch>
2020-02-12 15:54:17 -0500

    Add basic class PMT

---
+++ inc/Pmt.hh
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+// PMT Class definition
+//
+// mailto:ascarpell@bnl.gov
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef  __PMT_H
+#define __PMT_H
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+
+class PMT
+{
+  public:
+    PMT();
+    ~PMT();
+
+  private:
+};
+
+#endif __PMT_H
