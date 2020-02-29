50d85da4989506804802b255acd80917da2088e0
Andrea Scarpelli <Andrea Scarpelli>
2020-02-28 21:35:32 -0500

    macro for the analysis of noise

--- macro/noise_ana.cc
+++ macro/noise_ana.cc
@@ -5,7 +5,7 @@
 // "noise_run%d_00%d.root". you must change manually the variables run and subrun for now.
 //
 // Usage: root -l loadLib.cc noise_ana.cc
-// Out of the box it will produce a root file in the macro folder called "noise_run0_000.root"
+// Out of the box, it will produce a root file in the macro folder called "noise_run1264_001.root"
 // with the ADC distribution of all the pmts in data_dl1_run1264_1_20200227T235326-decoded.root
 // after baseline subtraction.
 //
@@ -24,103 +24,122 @@ using namespace std;
 
 //------------------------------------------------------------------------------
 
-PMT* locatePMT(int run, int board, int channel , vector<PMT*> pmts)
-{
-  int index = -1;
-  auto find = [&]( PMT *pmt )
-  {
-    return (pmt->getRun()==run && pmt->getBoard()==board
-                                                 && pmt->getChannel()==channel);
-  };
-
-  auto it = std::find_if( pmts.begin(), pmts.end(), find );
-  return *it;
-}
-
-//------------------------------------------------------------------------------
-
-void noise_ana(string filename="/media/disk_a/ICARUS/PMTDataMonitoring/data_dl1_run1264_1_20200227T235326-decoded.root")
+void noise_ana()
 {
 
-  int run=0.0;
-  int subrun=0.0;
-  int nboards=11;
-  int nchannels=16;
+  // CHANGE THE FILENAME BELOW IF WANT TO USE ONE OTHER FILE FOR YOUR ANALYSIS
+  string filename="/media/disk_a/ICARUS/PMTDataMonitoring/data_dl1_run1264_1_20200227T235326-decoded.root";
+  int run=1264; //<< TO BE CHANGE ACCORDING TO THE FILENAME
+  int subrun=1; //<< TO BE CHANGE ACCORDING TO THE FILENAME
 
-  //****************************************************************************
-  // Data structures definitions
-
-  vector<PMT*> pmts;
-
-  for( int board=0; board<nboards; board++ )
-  {
-    for( int channel=0; channel<nchannels; channel++ )
-    {
-      PMT *pmt = new PMT( run, board, channel );
-      pmts.push_back(pmt);
-    }
-  }
+  const int nboards=1;    // << This type of files in /PMTDataMonitoring/ have only one board. CHANGE ONLY IF YOU ARE LOOKING AT ONE OTHER TYPE OF FILE!
+  const int nchannels=16; // << DON'T CHANGE
 
   //****************************************************************************
   // Input
+  //
 
   string treename="caenv1730dump/events";
   TChain *tchain =  new TChain(treename.c_str());
 
+  // HERE you filename is loaded inside the macro to be processed!
   tchain->Add(filename.c_str());
 
-  // Set Branch address
   std::vector<std::vector<uint16_t> > *data=0; //unsigned short
   tchain->SetBranchAddress("fWvfmsVec", &data);
 
-  //****************************************************************************
-  // Event loop
 
-  // Loop over the events
+  //****************************************************************************
+  // Now we create and initialize an histogram per each board and channel
+  // This is an example, you can create more histograms using this model
+
+  TH1D *h_pmt_rms[nboards][nchannels];
+
+  for(int board=0; board<nboards; board++)
+  {
+    for(int channel=0; channel<nchannels; channel++)
+    {
+      //DEFINE HERE THE HISTOGRAMS. EACH HISTOGRAM SHOULD HAVE A DIFFERENT NAME
+      //TO AVOID CONFUSION WHEN YOU SAVE THEM TO FILE.
+      char hname[100];
+      sprintf(hname, "hnoise_run%d_00%d_board%d_channel%d", run, subrun, board, channel);
+      h_pmt_rms[board][channel]= new TH1D(hname, hname, 20, -20, 20);
+    }
+  }
+
+
+  //****************************************************************************
+  // Now it is time to loop over the events
+  //
+
   for(int e=0; e<tchain->GetEntries(); e++)
   {
     cout << "Processing event: " << e << endl;
 
+    // WE TAKE THE EVENT
     tchain->GetEvent(e);
 
-    const int n_channels = 16;
-    const int n_samples = (*data)[0].size();
-    const int n_boards = (*data).size()/n_channels;
-
-    for(int board=0; board<n_boards; board++)
+    for(int board=0; board<nboards; board++)
     {
-      for(int channel=0; channel<n_channels; channel++)
+      for(int channel=0; channel<nchannels; channel++)
       {
 
-        // Create the PMT object
+        // Create the WAVEFORM OBJECT
         Waveform *waveform = new Waveform(run, subrun ,e, board, channel);
-        waveform->loadData((*data).at(channel+n_channels*board));
 
-        PMT *pmt_temp = locatePMT(run, board, channel, pmts);
-        pmt_temp->loadWaveform(waveform);
+        // ONCE THE DATA ARE LOADED IN THE WAVEFORM OBJECT, BASELINE IS
+        // AUTOMATICALLY SUBSTRACTED. YOU CAN USE THE FUNCTION "getWaveform()"
+        // TO EXTRACT THE WAVEFORM VECTOR AFTER BASELINE SUBTRACTION OR
+        // "getRawWaveform()" BEFORE BASELINE SUBTRACTION.
+        waveform->loadData((*data).at(channel+nchannels*board));
 
-      } // end channel
-    } // end boards
+        //LOOP OVER THE RAW WAVEFORM, UNCOMMENT THE LOOP IF NEED TO DO IT
+        //for( float entry : waveform->getWaveform() )
+        //{
+        //  //DO SOMETHING
+        //}
 
-  } // end events
+        // Loop over the entries of the waveform after the baseline subtraction
+        // and fill the entries of the histogram h_pmt_rms
+        for( float entry : waveform->getWaveform() )
+        {
+          h_pmt_rms[board][channel]->Fill( entry );
+        }
+
+      } // channel
+    } // boards
+  } // event
+
+
 
   //****************************************************************************
-  // Now save the PMTs
+  // Output: create an output TFile and write the histogram in it
+  //
 
-  // Open TFile (remember to change the output filename if necessary)
   char ofilename[100]; sprintf(ofilename, "noise_run%d_00%d.root", run, subrun);
-  TFile ofile(ofilename, "RECREATE");
-  ofile.cd();
+  TFile ofile(ofilename, "RECREATE"); ofile.cd();
 
-  for(auto pmt_it : pmts)
+  for(int board=0; board<nboards; board++)
   {
-    cout << pmt_it->getBoard() << " " << pmt_it->getChannel() << endl;
-    TH1D* h_noise = pmt_it->getNoiseHist();
-    h_noise->Write();
+    for(int channel=0; channel<nchannels; channel++)
+    {
+      //Write the histogram to the output TFILE
+      h_pmt_rms[board][channel]->Write();
+    }
   }
 
   ofile.Close();
 
   cout << "All done" << endl;
+
+  //****************************************************************************
+  //
+  // Now you can open the output file "noise_run%d_00%d.root" using the command:
+  // root -l "noise_run%d_00%d.root". With the TBrowser you should be able to
+  // visualize all the histograms. Alternatively you can create one other root
+  // macro which import this TFILE and make TGraphs with the quantities you find
+  // more relevant.
+  //
+  //****************************************************************************
 
 } //end macro
