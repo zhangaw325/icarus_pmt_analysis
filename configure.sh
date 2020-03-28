e885da1326cd39b4489ccf69edd7667a1098bc62
Andrea Scarpelli <ascarpel@icarusgpvm04.fnal.gov>
2020-03-28 15:18:53 -0500

    Edit the configure.sh and loadLib.cc for gpvm

--- configure.sh
+++ configure.sh
@@ -1,6 +1,57 @@
 #!/bin/bash
 
+################################################################################
+#
+#
+################################################################################
+
+# Setup manually the EIGEN_INSTALL variable which is the most opportune for you
+export EIGEN_INSTALL=""
+
+# This part of the code is effective only on a fnal.gov domain
+if [ "${HOSTNAME#*.}" == "fnal.gov" ]; then
+
+  # Setup the general icaruscode ups packages
+  source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setup
+  source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
+
+  # Find the location of the eigen heders and replace the value set before
+  EIGEN_INSTALL="/cvmfs/larsoft.opensciencegrid.org/products/eigen/v3_3_5/include/eigen3/"
+
+  # Now setup the latest version of ROOT using ups producs
+  out=( "$(ups list -aK+ root | tail -1)" )
+  line=( $out )
+
+  remove_quotes()
+  {
+    var=$1
+    temp="${var%\"}"
+    temp="${temp#\"}"
+    echo "$temp"
+  }
+  version=$( remove_quotes ${line[1]} )
+  qualifier=$( remove_quotes ${line[3]} )
+
+  setup root -v ${version} -q ${qualifier}
+
+fi
+
+# Check if the EIGEN_INSTALL variable is configured
+if [ $EIGEN_INSTALL == "" ]; then
+  echo "DON'T FORGET TO SET THE EIGEN_INSTALL VARIABLE!"
+  return
+fi
+
+# Check if root is configured
+if [ $ROOTSYS == "" ]; then
+  echo "DON'T FORGET TO SET A ROOT VERSION!"
+  return
+fi
+
+# Get the current dir
 currentdir=$(pwd)
 
+# Add directories to the local path
 export PATH="$currentdir/build/:$PATH"
 export LD_LIBRARY_PATH="$currentdir/build/:$LD_LIBRARY_PATH"
+export PROJECT_PATH="$currentdir"
--- macro/loadLib.cc
+++ macro/loadLib.cc
@@ -10,24 +10,32 @@
 
 void loadLib(){
 
-  char cwd[1024];
-  getcwd(cwd, sizeof(cwd));
-
   //gStyle->SetPalette(kRainBow);
   //gStyle->SetOptFit(11111);
 
-  std::string currentdir( cwd );
-  std::string includepath=currentdir+"/../inc/";
-
-  cout << includepath << endl;
-
+  // Target a build folder
   gSystem->SetBuildDir("obj",true);
 
   // For some reasons ROOT has not a dictionary for vector of vectors
   gInterpreter->GenerateDictionary("vector<vector<unsigned short>>", "vector");
 
-  // Custom classes
+  char *eigen_install = getenv("EIGEN_INSTALL");
+  if(eigen_install == NULL)
+  {
+    cout << " Set the EIGEN_INSTALL variable! " << endl;
+  }
+  gInterpreter->AddIncludePath(eigen_install);
+
+  // Point to the location of the other libraries
+  // It assumes you use loadLib inside the macro folder
+  char cwd[1024];
+  getcwd(cwd, sizeof(cwd));
+  string currentdir( cwd );
+
+  string includepath=currentdir+"/../inc/";
   gInterpreter->AddIncludePath(includepath.c_str());
+
+  // Compile the custom classes
   gROOT->LoadMacro((currentdir+"/../src/Run.cc+").c_str());
   gROOT->LoadMacro((currentdir+"/../src/Waveform.cc+").c_str());
   gROOT->LoadMacro((currentdir+"/../src/Pmt.cc+").c_str());
